b3720168ba8b322ff293b9dcc4d30be76b2fc23c
Sanaya James <sanaya.j.james@gmail.com>
2026-04-15 16:57:10 -0700

    made test 3 pass. deleted unnecessary comments

--- main.py
+++ main.py
@@ -11,54 +11,49 @@ conn1 = sqlite3.connect('planets.db')
 pd.read_sql("""SELECT * FROM planets; """, conn1)
 
 # STEP 1
-# Replace None with your code
 df_no_moons = pd.read_sql("""
                           SELECT * FROM planets
                           WHERE num_of_moons = 0;
                           """, conn1)
 
-# print(df_no_moons)
+print(df_no_moons)
 
 # STEP 2
-# Replace None with your code
 df_name_seven = pd.read_sql("""
                             SELECT name, mass
                             FROM planets
                             WHERE length(name) = 7;
                             """, conn1)
 
-# print(df_name_seven)
+print(df_name_seven)
 
 ##### Part 2: Advanced Filtering #####
 
 # STEP 3
-# Replace None with your code
 df_mass = pd.read_sql("""
                       SELECT name, mass
                       FROM planets
                       WHERE mass <= 1.00;
                       """, conn1)
 
-# print(df_mass)
+print(df_mass)
 
 # STEP 4
-# Replace None with your code
 df_mass_moon = pd.read_sql("""
                            SELECT * FROM planets
                            WHERE num_of_moons >= 1 AND mass < 1.00;
                            """, conn1)
 
-# print(df_mass_moon)
+print(df_mass_moon)
 
 # STEP 5
-# Replace None with your code
 df_blue = pd.read_sql("""
                       SELECT name, color
                       FROM planets
                       WHERE color LIKE '%blue%';
                       """, conn1)
 
-# print(df_blue)
+print(df_blue)
 
 ##### Part 3: Ordering and Limiting #####
 
@@ -72,7 +67,6 @@ conn2 = sqlite3.connect('dogs.db')
 pd.read_sql("SELECT * FROM dogs;", conn2)
 
 # STEP 6
-# Replace None with your code
 df_hungry = pd.read_sql("""
                         SELECT name, age, breed
                         FROM dogs
@@ -80,10 +74,9 @@ df_hungry = pd.read_sql("""
                         ORDER BY age;
                         """, conn2)
 
-# print(df_hungry)
+print(df_hungry)
 
 # STEP 7
-# Replace None with your code
 df_hungry_ages = pd.read_sql("""
                              SELECT name, age, hungry
                              FROM dogs
@@ -91,10 +84,9 @@ df_hungry_ages = pd.read_sql("""
                              ORDER BY name;
                              """, conn2)
 
-# print(df_hungry_ages)
+print(df_hungry_ages)
 
 # STEP 8
-# Replace None with your code
 df_4_oldest = pd.read_sql("""
                           SELECT name, age, breed
                           FROM dogs
@@ -102,7 +94,7 @@ df_4_oldest = pd.read_sql("""
                           LIMIT 4
                           """, conn2)
 
-# print(df_4_oldest)
+print(df_4_oldest)
 
 
 ##### Part 4: Aggregation #####
@@ -118,37 +110,33 @@ pd.read_sql("""
 SELECT * FROM babe_ruth_stats; """, conn3)
 
 # STEP 9
-# Replace None with your code
 df_ruth_years = pd.read_sql("""
                             SELECT COUNT(*) AS total_years_played
                             FROM babe_ruth_stats;
                             """, conn3)
 
-# print(df_ruth_years)
+print(df_ruth_years)
 
 # STEP 10
-# Replace None with your code
 df_hr_total = pd.read_sql("""
                           SELECT SUM(HR) AS total_hrs
                           FROM babe_ruth_stats;
                           """, conn3)
 
-# print(df_hr_total)
+print(df_hr_total)
 
 ##### Part 5: Grouping and Aggregation #####
 
 # STEP 11
-# Replace None with your code
 df_teams_years = pd.read_sql("""
                              SELECT team, COUNT(team) AS number_years
                              FROM babe_ruth_stats
                              GROUP BY team;
                              """, conn3)
 
-# print(df_teams_years)
+print(df_teams_years)
 
 # STEP 12
-# Replace None with your code
 df_at_bats = pd.read_sql("""
                          SELECT team, AVG(at_bats) AS average_at_bats
                          FROM babe_ruth_stats
@@ -156,7 +144,7 @@ df_at_bats = pd.read_sql("""
                          HAVING AVG(at_bats) >= 200;
                          """, conn3)
 
-# print(df_at_bats)
+print(df_at_bats)
 
 
 conn1.close()
